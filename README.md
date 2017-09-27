6f715fba4655a822cb92d362badb86f5a0579d8c
André Bauer <andre.bauer@kiwigrid.com>
2017-09-27 09:19:57 +0200

    create config.json from entrypoint

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 container uses: https://github.com/lewg/pingdom-to-graphite
 
-* docker run -ti -v /data/.p2g/:/root/.p2g/ monotek/docker-pingdom-to-graphite
+* docker run -ti -e PINGDOM_USER="you@mail.com" -e PINGDOM_PASS="123456" -e PINGDOM_APIKEY="123456789" docker.kiwigrid.com/ops/pingdom-to-graphite
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 if [ "$1" = 'pingdom-to-graphite' ]; then
+
+    /usr/local/bundle/bin/pingdom-to-graphite init
+
+    sed -i -e "s#YOUR_USERNAME#${PINGDOM_USER}#g" -e "s#YOUR_PASSWORD#${PINGDOM_PASS}#g" -e "s#YOUR_API_KEY#${PINGDOM_APIKEY}#g" /root/.p2g/config.json
+
+    /usr/local/bundle/bin/pingdom-to-graphite init_checks
+
+    if [ "$?" != "0" ]; then
+	echo "error getting pingdom checks! are username, password and api key env vars set?"
+	exit 1
+    fi
+
     while true; do
 	/usr/local/bundle/bin/pingdom-to-graphite update
 	sleep 60
