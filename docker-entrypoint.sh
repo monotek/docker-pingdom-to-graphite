630ed64b01875fe04c3d28475ad5e75bfcb02f75
André Bauer <andre.bauer@kiwigrid.com>
2017-09-27 09:57:23 +0200

    added some infos to entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,18 +2,23 @@
 
 if [ "$1" = 'pingdom-to-graphite' ]; then
 
+    # create empty configfile - /root/.p2g/config.json
     /usr/local/bundle/bin/pingdom-to-graphite init
 
+    # add replace username, password and apikey in /root/.p2g/config.json
     sed -i -e "s#YOUR_USERNAME#${PINGDOM_USER}#g" -e "s#YOUR_PASSWORD#${PINGDOM_PASS}#g" -e "s#YOUR_API_KEY#${PINGDOM_APIKEY}#g" /root/.p2g/config.json
 
+    # add configured pingdom checks
     /usr/local/bundle/bin/pingdom-to-graphite init_checks
 
+    # exit if pingdom cant be accessed
     if [ "$?" != "0" ]; then
 	echo "error getting pingdom checks! are username, password and api key env vars set?"
 	exit 1
     fi
 
     while true; do
+	# get pingdom metrics
 	/usr/local/bundle/bin/pingdom-to-graphite update
 	sleep 60
     done
