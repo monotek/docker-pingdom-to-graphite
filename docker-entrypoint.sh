d4d0cf3fa9e64e2f4a8dfd21fcb2a08c90d4103a
André Bauer <andre.bauer@kiwigrid.com>
2017-09-28 12:51:37 +0200

    added query delay and pd_reges env vars

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,8 @@
 
 if [ "$1" = 'pingdom-to-graphite' ]; then
 
+    QUERY_DELAY="${QUERY_DELAY:=300}"
+
     # create empty configfile - /root/.p2g/config.json
     /usr/local/bundle/bin/pingdom-to-graphite init
 
@@ -9,7 +11,7 @@ if [ "$1" = 'pingdom-to-graphite' ]; then
     sed -i -e "s#YOUR_USERNAME#${PINGDOM_USER}#g" -e "s#YOUR_PASSWORD#${PINGDOM_PASS}#g" -e "s#YOUR_API_KEY#${PINGDOM_APIKEY}#g" /root/.p2g/config.json
 
     # add configured pingdom checks
-    /usr/local/bundle/bin/pingdom-to-graphite init_checks
+    /usr/local/bundle/bin/pingdom-to-graphite init_checks "${PD_REGEX}"
 
     # exit if pingdom cant be accessed
     if [ "$?" != "0" ]; then
@@ -18,8 +20,8 @@ if [ "$1" = 'pingdom-to-graphite' ]; then
     fi
 
     while true; do
-	# get pingdom metrics
 	/usr/local/bundle/bin/pingdom-to-graphite update
-	sleep 60
+	sleep "${QUERY_DELAY}"
     done
+
 fi
